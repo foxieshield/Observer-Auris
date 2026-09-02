b7282ab30eb028879f000e338d9faafc15921aa8
Manuel A. Arana <jhoysteve@yahoo.com.mx>
2026-09-02 12:57:54 -0600

    Revise survivor perks and add Autodidact
    Updated survivor perks with new names and descriptions, added Autodidact perk.

--- perks.js
+++ perks.js
@@ -1,75 +1,85 @@
 const perks = [
-
+     //-----survivor perks
     {
-        name: "Dead Hard",
+        name: "A Place For Us",
         type: "survivor",
-        description: "TEST DESCRIPTION",
+        description: "while healing another survivor, both gain elusive. whenever you finish healing the Obsession, you both gain elusive for 30s.",
         image: ""
     },
 
     {
-        name: "Lithe",
+        name: "ACE In The Hole",
         type: "survivor",
-        description: "TEST DESCRIPTION",
-        image: ""
-    },
-
-    {
-        name: "Sprint Burst",
-        type: "survivor",
-        description: "TEST DESCRIPTION",
+        description: "Whenever you loot an item from a chest, it will have: a Viscerla or lower rarity add-on & 100% chance for uncommon or lower rarity add-on",
         image: ""
     },
 
     {
         name: "Adrenaline",
         type: "survivor",
-        description: "TEST DESCRIPTION",
+        description: "When all generators are completed, if you are downed or injured, heal instantly. Then, ignoring exhausted: Gain 50% haste for 4s & gain exhausted for 40s",
         image: ""
     },
 
     {
-        name: "Iron Will",
+        name: "Aftercare",
         type: "survivor",
-        description: "TEST DESCRIPTION",
+        description: "You and up to 3 survivors see achothers auras whenever: you unhook them or they unhook you. You finish healing them, or they finish healing you. this effect lasts until unhooked",
         image: ""
     },
 
     {
-        name: "Kindred",
+        name: "Alert",
         type: "survivor",
-        description: "TEST DESCRIPTION",
+        description: "Whenever the killer breaks a pallet or beakable, or damages a generator: you hear a sound cue. you see their aura for 5s",
         image: ""
     },
 
     {
-        name: "Resilience",
+        name: "Any Means Necesary",
         type: "survivor",
-        description: "TEST DESCRIPTION",
+        description: "While next to a Dropped Pallet, use 'A', 'circle' or 'left Click' for 3s to reset it. you see the aura of dropped pallets",
         image: ""
     },
 
     {
-        name: "Windows of Opportunity",
+        name: "Apocalyptic Ingenuity",
         type: "survivor",
-        description: "TEST DESCRIPTION",
+        description: "After you rummage or unlock 1 chest, while next to a broken pallet, use 'A', 'circle' or 'left Click'for 3s to create fragile pallet. within 32m, you see the aura of broken pallets",
         image: ""
     },
 
     {
-        name: "Decisive Strike",
+        name: "Appraisal",
         type: "survivor",
-        description: "TEST DESCRIPTION",
+        description: "when the trial starts you earn 4 token. spend 1 token to rummage an empty chest up to 2 times per chest. rummage 80% faster",
         image: ""
     },
 
+   {
+    name: "Autodidact",
+    type: "survivor",
+
+    description: `
+You start the trial with 0 tokens, up to 5.
+Your good basic healing skill checks give 15% less progress.
+
+While healing another survivor without a med-kit:
+
+• Whenever you hit a basic skill check, earn 1 token.
+• For each token, you gain 10% more progress.
+• You cannot get great basic skill checks.
+`,
+
+    image: ""
+},
     {
         name: "Borrowed Time",
         type: "survivor",
         description: "TEST DESCRIPTION",
         image: ""
     },
-
+      //---------killer perks
     {
         name: "Hex: Ruin",
         type: "killer",
