a9fc508e8fd9a69f3046a5f06686e2af4bd3b277
Manuel A. Arana <jhoysteve@yahoo.com.mx>
2026-09-02 09:52:37 -0600

    Add perks data for survivors and killers

---
+++ perks.js
@@ -0,0 +1,108 @@
+const perks = [
+
+    {
+        name: "Dead Hard",
+        type: "survivor",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Lithe",
+        type: "survivor",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Sprint Burst",
+        type: "survivor",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Adrenaline",
+        type: "survivor",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Iron Will",
+        type: "survivor",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Kindred",
+        type: "survivor",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Resilience",
+        type: "survivor",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Windows of Opportunity",
+        type: "survivor",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Decisive Strike",
+        type: "survivor",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Borrowed Time",
+        type: "survivor",
+        description: "TEST DESCRIPTION",
+        image: ""
+    }
+
+    {
+        name: "Hex: Ruin",
+        type: "killer",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Pop Goes the Weasel",
+        type: "killer",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Barbecue & Chilli",
+        type: "killer",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "A Nurse's Calling",
+        type: "killer",
+        description: "TEST DESCRIPTION",
+        image: ""
+    },
+
+    {
+        name: "Sloppy Butcher",
+        type: "killer",
+        description: "TEST DESCRIPTION",
+        image: ""
+    }
+  
+];
