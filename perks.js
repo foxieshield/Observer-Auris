c4e3b87e37a19f6e6ceaae9e5587cded7c4bb35f
Manuel A. Arana <jhoysteve@yahoo.com.mx>
2026-09-02 13:26:34 -0600

    Refactor perk descriptions for better readability

--- perks.js
+++ perks.js
@@ -3,56 +3,86 @@ const perks = [
     {
         name: "A Place For Us",
         type: "survivor",
-        description: "while healing another survivor, both gain elusive. whenever you finish healing the Obsession, you both gain elusive for 30s.",
+        description: `
+        while healing another survivor, both gain elusive. 
+        whenever you finish healing the Obsession, you both gain elusive for 30s.
+        `,
         image: ""
     },
 
     {
         name: "ACE In The Hole",
         type: "survivor",
-        description: "Whenever you loot an item from a chest, it will have: a Viscerla or lower rarity add-on & 100% chance for uncommon or lower rarity add-on",
+        description: `
+        Whenever you loot an item from a chest, it will have: 
+        •a Viscerla or lower rarity add-on.
+        •100% chance for uncommon or lower rarity add-on.
+        `,
         image: ""
     },
 
     {
         name: "Adrenaline",
         type: "survivor",
-        description: "When all generators are completed, if you are downed or injured, heal instantly. Then, ignoring exhausted: Gain 50% haste for 4s & gain exhausted for 40s",
+        description: `
+        When all generators are completed, if you are downed or injured, heal instantly. 
+        Then, ignoring exhausted:
+        •Gain 50% haste for 4s 
+        •gain exhausted for 40s
+        `,
         image: ""
     },
 
     {
         name: "Aftercare",
         type: "survivor",
-        description: "You and up to 3 survivors see achothers auras whenever: you unhook them or they unhook you. You finish healing them, or they finish healing you. this effect lasts until unhooked",
+        description: `
+        You and up to 3 survivors see achothers auras whenever: 
+        •you unhook them or they unhook you. You finish healing them, or they finish healing you. 
+        this effect lasts until unhooked
+        `,
         image: ""
     },
 
     {
         name: "Alert",
         type: "survivor",
-        description: "Whenever the killer breaks a pallet or beakable, or damages a generator: you hear a sound cue. you see their aura for 5s",
+        description: `
+        Whenever the killer breaks a pallet or beakable, or damages a generator: 
+        •you hear a sound cue. you see their aura for 5s
+        `,
         image: ""
     },
 
     {
         name: "Any Means Necesary",
         type: "survivor",
-        description: "While next to a Dropped Pallet, use 'A', 'circle' or 'left Click' for 3s to reset it. you see the aura of dropped pallets",
+        description: `
+        While next to a Dropped Pallet, use 'A', 'circle' or 'left Click' for 3s to reset it. 
+        you see the aura of dropped pallets
+        `,
         image: ""
     },
 
     {
         name: "Apocalyptic Ingenuity",
         type: "survivor",
-        description: "After you rummage or unlock 1 chest, while next to a broken pallet, use 'A', 'circle' or 'left Click'for 3s to create fragile pallet. within 32m, you see the aura of broken pallets",
+        description: `
+        After you rummage or unlock 1 chest, while next to a broken pallet, 
+        use 'A', 'circle' or 'left Click'for 3s to create fragile pallet. 
+        within 32m, you see the aura of broken pallets
+        `,
         image: ""
     },
 
     {
         name: "Appraisal",
         type: "survivor",
-        description: "when the trial starts you earn 4 token. spend 1 token to rummage an empty chest up to 2 times per chest. rummage 80% faster",
+        description: `
+        when the trial starts you earn 4 token. 
+        spend 1 token to rummage an empty chest up to 2 times per chest. 
+        rummage 80% faster
+        `,
         image: ""
     },
 
@@ -61,57 +91,81 @@ const perks = [
     type: "survivor",
 
     description: `
-You start the trial with 0 tokens, up to 5.
-Your good basic healing skill checks give 15% less progress.
+         You start the trial with 0 tokens, up to 5.
+         Your good basic healing skill checks give 15% less progress.
 
-While healing another survivor without a med-kit:
+         While healing another survivor without a med-kit:
 
-• Whenever you hit a basic skill check, earn 1 token.
-• For each token, you gain 10% more progress.
-• You cannot get great basic skill checks.
-`,
+        • Whenever you hit a basic skill check, earn 1 token.
+         • For each token, you gain 10% more progress.
+          • You cannot get great basic skill checks.
+          `,
 
     image: ""
 },
     {
-        name: "Borrowed Time",
+        name: "Babysitter",
         type: "survivor",
-        description: "TEST DESCRIPTION",
+        description: `
+        whenever you unhook a survivor:
+        •they gain haste 10% for 30s
+        •they leave no Pools of blood for 30s
+        •they leave no scratch marks for 30s
+        •You see the killers aura for 8s
+        `,
         image: ""
     },
       //---------killer perks
     {
-        name: "Hex: Ruin",
-        type: "killer",
-        description: "TEST DESCRIPTION",
-        image: ""
-    },
-
-    {
-        name: "Pop Goes the Weasel",
-        type: "killer",
-        description: "TEST DESCRIPTION",
-        image: ""
-    },
-
-    {
-        name: "Barbecue & Chilli",
-        type: "killer",
-        description: "TEST DESCRIPTION",
-        image: ""
-    },
-
-    {
         name: "A Nurse's Calling",
         type: "killer",
-        description: "TEST DESCRIPTION",
+        description: `
+        Within 32m of survivors who are healing or being healed, 
+        see their auras
+        `,
         image: ""
     },
 
     {
-        name: "Sloppy Butcher",
+        name: "Agitation",
         type: "killer",
-        description: "TEST DESCRIPTION",
+        description: `
+        While carrying a survivor:
+        •You gain 18% haste
+        •Your terror radius is 12m larger
+        `,
+        image: ""
+    },
+
+    {
+        name: "Alien Instinct",
+        type: "killer",
+        description: `
+        Whenever you hook a survivor, the farthest injured survivor:
+        •gains Oblivious for 60s
+        •You see their aura for 8s
+     `,
+        image: ""
+    },
+
+    {
+        name: "All Shaking Thunder",
+        type: "killer",
+        description: `
+        Whenever you fall from atleast 1.25m, 
+        your lunge attacks are 75% longer for 25s. 
+        cooldown:5s
+        `,
+        image: ""
+    },
+
+    {
+        name: "Awakened Awareness",
+        type: "killer",
+        description: `
+        While carrying a survivor, within 20m of you,
+        you see the auras of other survivors
+        `,
         image: ""
     }
   
